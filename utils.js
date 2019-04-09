1890c35d9d4602bfb9a29a9a42e9c5b39ce2345f
TyrionJYQ <1003518719@qq.com>
2019-04-09 11:20:50 +0800

    add createObserver func

--- utils.js
+++ utils.js
@@ -1,6 +1,7 @@
 
 module.exports = {
-  walkDOM: walkDOM
+  walkDOM: walkDOM,
+  createObserver: createObserver
 }
 
 /*
@@ -15,4 +16,43 @@ function walkDOM(target) {
       walkDOM(target.firstChild);
     }
   } while (target = target.nextSibling)
+}
+
+/*
+* @description 创建观察者 
+ */
+function createObserver() {
+  var observer = {
+    addSubscriber: function (callback) {
+      if (typeof callback === 'function') {
+        this.subscribers[this.subscribers.length] = callback;
+      }
+    },
+
+    removeSubscriber: function (callback) {
+      for (var i = 0; i < this.subscribers.length; i++) {
+        if (this.subscribers[i] === callback) {
+          delete this.subscribers[i]
+        }
+      }
+    },
+
+    publish: function (what) {
+      for (var i = 0; i < this.subscribers.length; i++) {
+        if (typeof this.subscribers[i] === 'function') {
+          this.subscribers[i](what);
+        }
+      }
+    },
+
+    make: function (o) { // turns an object into a publisher
+      for (var i in this) {
+        if (this.hasOwnProperty(i)) {
+          o[i] = this[i];
+          o.subscribers = [];
+        }
+      }
+    }
+  }
+  return observer; 
 }
